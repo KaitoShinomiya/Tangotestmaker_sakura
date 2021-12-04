638eb03777fffbfa70381c00d6152d221b47435f
shinomiyakaito <shinomiyakaito@icloud.com>
2021-12-05 01:15:58 +0900

    一旦コミット

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ def result_check():
                                return_table=return_table)
 
 
-@app.route("/login", methods=["GET", "POST"])
+''''@app.route("/login", methods=["GET", "POST"])
 def user_login():
     return render_template("login.html")
 
@@ -134,7 +134,7 @@ def user_login():
 @app.route("/data_upload")
 def data_upload():
     return render_template("data_upload.html")
-
+'''
 
 @app.route("/register_result", methods=["POST"])
 def register():
